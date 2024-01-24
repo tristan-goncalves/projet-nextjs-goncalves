aa34f86b9f895c6e08083987141a8397ff4df773
Tristan <tristan.goncalves25@gmail.com>
2024-01-24 09:10:03 +0100

    commit base

--- app/api/create-evenement-table/route.ts
+++ app/api/create-evenement-table/route.ts
@@ -4,16 +4,9 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   try {
     const result =
-      await sql`CREATE TABLE EVENEMENTS ( 
-        id integer primary key, 
-        intitule varchar(255), 
-        petite_description varchar(255), 
-        grande_description varchar(255),
-        date varchar(255),
-        lieu varchar(255),
-        intervenants varchar(255) );`;
+      await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
-} 
+}
