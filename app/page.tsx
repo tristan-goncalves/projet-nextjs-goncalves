392d006dd58c9f29a8f1b11b05c99545f0d6b3c7
Tristan <tristan.goncalves25@gmail.com>
2024-01-24 10:29:56 +0100

    ajout du formulaire

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,28 @@ const Accueil = () => {
           <li><Link href="/about">À propos</Link></li>
         </ul>
       </nav>
+
+    <h2>Ajout d'un événement</h2>
+    <form action="/submit_event" method="post">
+      <label for="intitule">Intitulé de l'événement:</label>
+      <input type="text" id="intitule" name="intitule" required></input><br></br>
+
+      <label for="resume">Résumé de l'événement:</label>
+      <textarea id="resume" name="resume" rows="4" required></textarea><br></br>
+
+      <label for="description">Description détaillée de l'événement:</label>
+      <textarea id="description" name="description" rows="8" required></textarea><br></br>
+
+      <label for="date">Date de l'événement:</label>
+      <input type="date" id="date" name="date" required></input><br></br>
+
+      <label for="intervenants">Intervenants:</label>
+      <input type="text" id="intervenants" name="intervenants" required></input><br></br>
+      
+      <button type="submit">Créer l'événement</button>
+
+    </form>
+  
     </div>
   );
 };
