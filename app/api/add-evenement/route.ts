3ab2d40784bc054ec984eae64bf959acdd71ee02
Tristan <tristan.goncalves25@gmail.com>
2024-01-24 09:57:23 +0100

    droppp

--- app/api/add-evenement/route.ts
+++ app/api/add-evenement/route.ts
@@ -4,8 +4,8 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
  
   try {
-    await sql`INSERT INTO EVENEMENTS (intitule, petite_description, grande_description, date, lieu, intervenants)
-    VALUES ('Conférence sur l avenir de la technologie', 'Découvrez les dernières tendances technologiques', 'Une conférence passionnante sur l évolution rapide de la technologie et son impact sur notre avenir. Venez explorer les nouvelles frontières de l innovation avec nos experts renommés.', '2024-03-15', 'Palais des Congrès', ARRAY['Sophie Martin', 'François Dupont']);`;
+    await sql`
+    `;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
--- app/api/create-evenement-table/route.ts
+++ app/api/create-evenement-table/route.ts
@@ -4,14 +4,8 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   try {
     const result =
-      await sql`CREATE TABLE IF NOT EXISTS EVENEMENTS ( 
-        id serial primary key, 
-        intitule text, 
-        petite_description text, 
-        grande_description text,
-        date date,
-        lieu text,
-        intervenants text[] );`;
+      await sql`DROP TABLE IF EXISTS EVENEMENTS;
+      `;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
