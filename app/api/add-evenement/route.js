0f5292029b1928966da50e34e8e63428dd1b9518
Tristan <tristan.goncalves25@gmail.com>
2024-01-24 16:47:03 +0100

    1.0

--- app/api/add-evenement/route.js
+++ app/api/add-evenement/route.js
@@ -1,28 +1,30 @@
 import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
+import { NextApiRequest, NextApiResponse } from 'next';
 import pgPromise from 'pg-promise';
 
 const pgp = pgPromise();
 const db = pgp(process.env.DATABASE_URL);
- 
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-      try {
-        const { intitule, resume, description, date, lieu, intervenants } = req.body;
-  
-        // Exécutez la requête SQL pour insérer les données dans votre base de données
-        const result = await db.one(`INSERT INTO EVENEMENTS (intitule, petite_description, grande_description, date, lieu, intervenants)
-        VALUES($1, $2, $3, $4, $5, $6) RETURNING *`, [intitule, resume, description, date, lieu, intervenants]);
 
-        console.log('Données insérées avec succès:', result);
-  
-        res.status(200).json({ success: true, data: result });
-        return;
-      } catch (error) {
-        console.error('Erreur lors de l\'insertion des données:', error);
-        res.status(500).json({ success: false, error: 'Erreur serveur' });
-      }
-    } else {
-      res.status(405).json({ success: false, error: 'Méthode non autorisée' });
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    try {
+      const { intitule, resume, description, date, lieu, intervenants } = req.body;
+
+      // Exécutez la requête SQL pour insérer les données dans votre base de données
+      const result = await db.one(
+        `INSERT INTO EVENEMENTS (intitule, petite_description, grande_description, date, lieu, intervenants)
+         VALUES($1, $2, $3, $4, $5, $6) RETURNING *`,
+        [intitule, resume, description, date, lieu, intervenants]
+      );
+
+      console.log('Données insérées avec succès:', result);
+
+      res.status(200).json({ success: true, data: result });
+    } catch (error) {
+      console.error('Erreur lors de l\'insertion des données:', error);
+      res.status(500).json({ success: false, error: 'Erreur serveur' });
     }
+  } else {
+    res.status(405).json({ success: false, error: 'Méthode non autorisée' });
   }
+}
