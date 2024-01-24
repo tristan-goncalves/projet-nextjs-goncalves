3443510ca26d3e29f2d1fc39d17ad33e9dc734c2
Tristan <tristan.goncalves25@gmail.com>
2024-01-24 09:46:59 +0100

    ajout table evenement

--- app/api/create-evenement-table/route.ts
+++ app/api/create-evenement-table/route.ts
@@ -4,7 +4,14 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   try {
     const result =
-      await sql`DROP TABLE IF EXISTS PETS;`;
+      await sql`CREATE TABLE IF NOT EXISTS EVENEMENTS ( 
+        id serial primary key, 
+        intitule text, 
+        petite_description text, 
+        grande_description text,
+        date date,
+        lieu text,
+        intervenants text[] );`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
