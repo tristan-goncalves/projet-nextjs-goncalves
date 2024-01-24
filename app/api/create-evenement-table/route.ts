5f238053a4578be855955607a6d2cf33a231c437
Tristan <tristan.goncalves25@gmail.com>
2024-01-24 16:57:10 +0100

    1.3

--- app/api/create-evenement-table/route.ts
+++
@@ -1,8 +0,0 @@
-import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
-import pgPromise from 'pg-promise';
-
-const pgp = pgPromise();
-const db = pgp(process.env.DATABASE_URL);
-
-export { db };
