9f1cefce50c8d8bb6b68d2c5e7109158cc3a9511
Tristan <tristan.goncalves25@gmail.com>
2024-01-24 10:54:01 +0100

    form essai

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,13 @@ const Accueil = () => {
       </nav>
 
       <h2>Ajout d'un événement</h2><br />
-      
+      <form action="/send-data-here" method="post">
+  <label for="first">First name:</label>
+  <input type="text" id="first" name="first" />
+  <label for="last">Last name:</label>
+  <input type="text" id="last" name="last" />
+  <button type="submit">Submit</button>
+</form>
     </div>
   );
 };
