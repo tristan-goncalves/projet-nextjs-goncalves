ea1f51cb57ab1f8438552121ca6c0352dbe7d1f4
Tristan <tristan.goncalves25@gmail.com>
2024-01-24 10:42:18 +0100

    le commit sans le form pour test t'as vu

--- app/page.tsx
+++ app/page.tsx
@@ -13,25 +13,7 @@ const Accueil = () => {
         </ul>
       </nav>
 
-      <h2>Ajout d'un événement</h2>
-      <form action="/api/add-evenement" method="post">
-        <label htmlFor="intitule">Intitulé de l'événement:</label>
-        <input type="text" id="intitule" name="intitule" required /><br />
-
-        <label htmlFor="resume">Résumé de l'événement:</label>
-        <textarea id="resume" name="resume" rows="4" required></textarea><br />
-
-        <label htmlFor="description">Description détaillée de l'événement:</label>
-        <textarea id="description" name="description" rows="8" required></textarea><br />
-
-        <label htmlFor="date">Date de l'événement:</label>
-        <input type="date" id="date" name="date" required /><br />
-
-        <label htmlFor="intervenants">Intervenants:</label>
-        <input type="text" id="intervenants" name="intervenants" required /><br />
-
-        <button type="submit">Créer l'événement</button>
-      </form>
+      
     </div>
   );
 };
