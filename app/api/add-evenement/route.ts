32693b301af9fe7d2b095926a4edae6d2b4a0e53
Tristan <tristan.goncalves25@gmail.com>
2024-01-24 09:32:35 +0100

    correction 3

--- app/api/add-evenement/route.ts
+++ app/api/add-evenement/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
  
   try {
-    await sql`INSERT INTO TB_EVENEMENT (intitule, petite_description, grande_description, date, lieu, intervenants)
+    await sql`INSERT INTO EVENEMENTS (intitule, petite_description, grande_description, date, lieu, intervenants)
     VALUES ('Conférence sur l avenir de la technologie', 'Découvrez les dernières tendances technologiques', 'Une conférence passionnante sur l évolution rapide de la technologie et son impact sur notre avenir. Venez explorer les nouvelles frontières de l innovation avec nos experts renommés.', '2024-03-15', 'Palais des Congrès', ARRAY['Sophie Martin', 'François Dupont']);`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
