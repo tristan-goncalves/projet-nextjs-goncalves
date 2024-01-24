918a0105d4cce5f225dee3be9ce1472b9d5824d9
Tristan <tristan.goncalves25@gmail.com>
2024-01-24 09:44:40 +0100

    pets

--- app/api/create-evenement-table/route.ts
+++ app/api/create-evenement-table/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   try {
     const result =
-      await sql`DROP TABLE IF EXISTS EVENEMENTS;`;
+      await sql`DROP TABLE IF EXISTS PETS;`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
