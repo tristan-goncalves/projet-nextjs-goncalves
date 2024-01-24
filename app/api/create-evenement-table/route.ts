aa11466328d86eb9d1df90fddfe7fe50ca3f3b8f
Tristan <tristan.goncalves25@gmail.com>
2024-01-24 10:16:04 +0100

    delete la table

--- app/api/create-evenement-table/route.ts
+++ app/api/create-evenement-table/route.ts
@@ -4,16 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   try {
     const result =
-      await sql`CREATE TABLE IF NOT EXISTS EVENEMENTS ( 
-        id serial primary key, 
-        intitule text, 
-        petite_description text, 
-        grande_description text,
-        date date,
-        lieu text,
-        intervenants text[] );
-        
-        SELECT * FROM EVENEMENTS;`;
+      await sql`SELECT * FROM EVENEMENTS;`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
