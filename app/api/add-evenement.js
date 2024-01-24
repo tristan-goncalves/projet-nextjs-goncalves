be001ff1833336da351ad1696a601ba79c9e1e48
Tristan <tristan.goncalves25@gmail.com>
2024-01-24 17:21:56 +0100

    postgres

--- app/api/add-evenement.js
+++ app/api/add-evenement.js
@@ -4,7 +4,7 @@ import pgPromise from 'pg-promise';
 import cors from 'cors';
 
 const pgp = pgPromise();
-const db = pgp(process.env.POSTGRES_DATABASE);
+const db = pgp(process.env.POSTGRES_URL);
 const corsMiddleware = cors();
 
 export default async function handler(req, res) {
