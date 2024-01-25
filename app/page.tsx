3a796f9553c5b2d9f22c91a44af70828956a8a5b
Tristan <tristan.goncalves25@gmail.com>
2024-01-25 10:44:17 +0100

    try de commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
 // import React, { useState } from 'react';
 // import { sql } from '@vercel/postgres';
 // import { NextResponse } from 'next/server';
+"use client"; // This is a client component
+import React, { useEffect, useState } from 'react';
 
 const Accueil = () => {
-  
+
+  const [apiData, setApiData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/hello');
+        const data = await response.json();
+        setApiData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>API Response:</h1>
+      {apiData ? (
+        <pre>{JSON.stringify(apiData, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default Accueil;
